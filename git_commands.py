e1fa403d659d993fed331a871697e8854cf701f1
omurbek <omurbeksatarov46@gmail>
2023-08-01 19:28:30 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+# git - Система управления версиями кода
+
+
+# github - сайт, которрый дает хранилище для кода
+
+# также есть gitlab,bitbucket 
+
+# репозитрий - папка с проектом с которой может git 
+
+# ssh-keygen - создание ключа 
+
+# git config --globall user.name "your-name"
+#git config --globall user.email "your-email"
+# указание 
